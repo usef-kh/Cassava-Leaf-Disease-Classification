3be08dee08bfa5be17b3cd5138457b30923e9354
usef-kh <yousif.khaireddin@gmail.com>
2020-12-13 18:50:16 -0500

    batchnorm & dropout

--- models/vgg.py
+++ models/vgg.py
@@ -1,12 +1,13 @@
 import torch.nn as nn
 import torch.nn.functional as F
+import torchvision
 
 
 class Vgg(nn.Module):
     def __init__(self):
         super().__init__()
 
-        self.conv1a = nn.Conv2d(in_channels=3, out_channels=64, kernel_size=3, padding=1)
+        self.conv1a = nn.Conv2d(in_channels=1, out_channels=64, kernel_size=3, padding=1)
         self.conv1b = nn.Conv2d(64, out_channels=64, kernel_size=3, padding=1)
 
         self.conv2a = nn.Conv2d(64, 128, 3, padding=1)
@@ -18,10 +19,19 @@ class Vgg(nn.Module):
         self.conv4a = nn.Conv2d(256, 512, 3, padding=1)
         self.conv4b = nn.Conv2d(512, 512, 3, padding=1)
 
-        self.conv5a = nn.Conv2d(512, 512, 3, padding=1)
-        self.conv5b = nn.Conv2d(512, 512, 3, padding=1)
-
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.bn1a = nn.BatchNorm2d(64)
+        self.bn1b = nn.BatchNorm2d(64)
+
+        self.bn2a = nn.BatchNorm2d(128)
+        self.bn2b = nn.BatchNorm2d(128)
+
+        self.bn3a = nn.BatchNorm2d(256)
+        self.bn3b = nn.BatchNorm2d(256)
+
+        self.bn4a = nn.BatchNorm2d(512)
+        self.bn4b = nn.BatchNorm2d(512)
 
         self.lin1 = nn.Linear(512 * 6 * 8, 4096)
         self.lin2 = nn.Linear(4096, 4096)
@@ -30,29 +40,26 @@ class Vgg(nn.Module):
         self.drop = nn.Dropout(p=0.2)
 
     def forward(self, x):
-        x = F.relu(self.conv1a(x))
-        x = F.relu(self.conv1b(x))
+        x = F.relu(self.bn1a(self.conv1a(x)))
+        x = F.relu(self.bn1b(self.conv1b(x)))
         x = self.pool(x)
 
-        x = F.relu(self.conv2a(x))
-        x = F.relu(self.conv2b(x))
+        x = F.relu(self.bn2a(self.conv2a(x)))
+        x = F.relu(self.bn2b(self.conv2b(x)))
         x = self.pool(x)
 
-        x = F.relu(self.conv3a(x))
-        x = F.relu(self.conv3b(x))
+        x = F.relu(self.bn3a(self.conv3a(x)))
+        x = F.relu(self.bn3b(self.conv3b(x)))
         x = self.pool(x)
 
-        x = F.relu(self.conv4a(x))
-        x = F.relu(self.conv4b(x))
-        x = self.pool(x)
-
-        x = F.relu(self.conv5a(x))
-        x = F.relu(self.conv5b(x))
+        x = F.relu(self.bn4a(self.conv4a(x)))
+        x = F.relu(self.bn4b(self.conv4b(x)))
         x = self.pool(x)
 
         x = x.view(-1, 512 * 6 * 8)
-        x = F.relu(self.lin1(x))
-        x = F.relu(self.lin2(x))
+        x = F.relu(self.drop(self.lin1(x)))
+        x = F.relu(self.drop(self.lin2(x)))
         x = self.lin3(x)
 
         return x
+
